417228dff4c1aab0229f09ada592ad55fff11f2c
Kyna2k <huynobi1809@gmail.com>
2023-07-15 01:54:32 +0700

    hi

---
+++ fakemyfpl/app/Http/Controllers/KhoaHocController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+class KhoaHocController extends Controller{
+
+
+    public function __construct()
+    {
+        
+    }
+    public function index()
+    {
+        
+    }
+    //Hien thi danh sach khoa hoc
+    public function showDanhSachKhoaHoc()
+    {
+        return "Cai nganh l";
+    }
+}
