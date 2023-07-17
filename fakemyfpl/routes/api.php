7a7fe6bbb04668d022c57f3e6c89d3ea42ffd646
Kyna2k <huynobi1809@gmail.com>
2023-07-18 00:35:13 +0700

    da

--- fakemyfpl/routes/api.php
+++ fakemyfpl/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/test',function(Request $request){
+    $result = json_decode($request->getContent(), true);
+    return ["Mess"=>  "Ngu vl",
+            "Data" => $result];
+});
